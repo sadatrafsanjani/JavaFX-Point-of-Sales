f49fa44b25eef8e622dda0012276aa0516afbba6
Bindambc <mauriciobinda@hotmail.com>
2020-02-29 01:23:04 -0300

    pom.xml and code updates

--- src/main/java/com/rafsan/inventory/model/EmployeeModel.java
+++ src/main/java/com/rafsan/inventory/model/EmployeeModel.java
@@ -1,113 +1,123 @@
 package com.rafsan.inventory.model;
 
-import com.rafsan.inventory.HibernateUtil;
-import com.rafsan.inventory.dao.EmployeeDao;
-import com.rafsan.inventory.entity.Employee;
 import java.util.List;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
 
+import com.rafsan.inventory.HibernateUtil;
+import com.rafsan.inventory.dao.EmployeeDao;
+import com.rafsan.inventory.entity.Employee;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class EmployeeModel implements EmployeeDao {
 
-    private static Session session;
+	private static Session session;
 
-    @Override
-    public ObservableList<Employee> getEmployees() {
+	@Override
+	public ObservableList<Employee> getEmployees() {
 
-        ObservableList<Employee> list = FXCollections.observableArrayList();
+		ObservableList<Employee> list = FXCollections.observableArrayList();
 
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        List<Employee> employees = session.createQuery("from Employee").list();
-        session.beginTransaction().commit();
-        employees.stream().forEach(list::add);
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		List<Employee> employees = session.createQuery("from Employee").list();
+		session.getTransaction().commit();
+		session.close();
+		employees.stream().forEach(list::add);
 
-        return list;
-    }
+		return list;
+	}
 
-    @Override
-    public Employee getEmployee(long id) {
+	@Override
+	public Employee getEmployee(long id) {
 
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Employee employee = session.get(Employee.class, id);
-        session.getTransaction().commit();
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Employee employee = session.get(Employee.class, id);
+		session.getTransaction().commit();
+		session.close();
 
-        return employee;
-    }
-    
-    @Override
-    public String getEmployeeType(String username){
-    
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Query query = session.createQuery("from Employee where userName = :username");
-        query.setParameter("username", username);
-        Employee employee = (Employee) query.uniqueResult();
-        session.getTransaction().commit();
-       
-        return employee.getType();
-    }
+		return employee;
+	}
 
-    @Override
-    public void saveEmployee(Employee employee) {
+	@Override
+	public String getEmployeeType(String username) {
 
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        session.save(employee);
-        session.getTransaction().commit();
-    }
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from Employee where userName = :username");
+		query.setParameter("username", username);
+		Employee employee = (Employee) query.uniqueResult();
+		session.getTransaction().commit();
+		session.close();
+		return employee.getType();
+	}
 
-    @Override
-    public void updateEmployee(Employee employee) {
+	@Override
+	public void saveEmployee(Employee employee) {
 
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Employee e = session.get(Employee.class, employee.getId());
-        e.setFirstName(employee.getFirstName());
-        e.setLastName(employee.getLastName());
-        e.setUserName(employee.getUserName());
-        e.setPassword(employee.getPassword());
-        e.setPhone(employee.getPhone());
-        e.setAddress(employee.getAddress());
-        session.getTransaction().commit();
-    }
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.save(employee);
+		session.getTransaction().commit();
+		session.close();
+	}
 
-    @Override
-    public void deleteEmployee(Employee employee) {
-        
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Employee e = session.get(Employee.class, employee.getId());
-        session.delete(e);
-        session.getTransaction().commit();
-    }
-    
-    @Override
-    public boolean checkUser(String username) {
+	@Override
+	public void updateEmployee(Employee employee) {
 
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Query query = session.createQuery("from Employee where userName = :username");
-        query.setParameter("username", username);
-        Employee employee = (Employee) query.uniqueResult();
-        session.getTransaction().commit();
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Employee e = session.get(Employee.class, employee.getId());
+		e.setFirstName(employee.getFirstName());
+		e.setLastName(employee.getLastName());
+		e.setUserName(employee.getUserName());
+		e.setPassword(employee.getPassword());
+		e.setPhone(employee.getPhone());
+		e.setAddress(employee.getAddress());
+		session.getTransaction().commit();
+		session.close();
+	}
 
-        return employee != null;
-    }
-    
-    @Override
-    public boolean checkPassword(String username, String password) {
+	@Override
+	public void deleteEmployee(Employee employee) {
 
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Query query = session.createQuery("from Employee where userName = :username");
-        query.setParameter("username", username);
-        Employee employee = (Employee) query.uniqueResult();
-        session.getTransaction().commit();
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Employee e = session.get(Employee.class, employee.getId());
+		session.delete(e);
+		session.getTransaction().commit();
+		session.close();
+	}
 
-        return employee.getPassword().equals(password);
-    }
+	@Override
+	public boolean checkUser(String username) {
+
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from Employee where userName = :username");
+		query.setParameter("username", username);
+		Employee employee = (Employee) query.uniqueResult();
+		session.getTransaction().commit();
+		session.close();
+
+		return employee != null;
+	}
+
+	@Override
+	public boolean checkPassword(String username, String password) {
+
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from Employee where userName = :username");
+		query.setParameter("username", username);
+		Employee employee = (Employee) query.uniqueResult();
+		session.getTransaction().commit();
+		session.close();
+
+		return employee.getPassword().equals(password);
+	}
 }
--- src/main/java/com/rafsan/inventory/model/PurchaseModel.java
+++ src/main/java/com/rafsan/inventory/model/PurchaseModel.java
@@ -1,68 +1,81 @@
 package com.rafsan.inventory.model;
 
+import java.util.List;
+
+import org.hibernate.Session;
+
 import com.rafsan.inventory.HibernateUtil;
 import com.rafsan.inventory.dao.PurchaseDao;
 import com.rafsan.inventory.entity.Purchase;
-import java.util.List;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import org.hibernate.Session;
 
 public class PurchaseModel implements PurchaseDao {
 
-    private static Session session;
-    
-    @Override
-    public ObservableList<Purchase> getPurchases() {
-        
-        ObservableList<Purchase> list = FXCollections.observableArrayList();
+	private static Session session;
 
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        List<Purchase> products = session.createQuery("from Purchase").list();
-        session.beginTransaction().commit();
-        products.stream().forEach(list::add);
+	@Override
+	public ObservableList<Purchase> getPurchases() {
 
-        return list;
-    }
+		ObservableList<Purchase> list = FXCollections.observableArrayList();
 
-    @Override
-    public Purchase getPurchase(long id) {
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Purchase purchase = session.get(Purchase.class, id);
-        session.getTransaction().commit();
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		List<Purchase> products = session.createQuery("from Purchase").list();
+		session.getTransaction().commit();
 
-        return purchase;
-    }
+		session.close();
+		products.stream().forEach(list::add);
 
-    @Override
-    public void savePurchase(Purchase purchase) {
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        session.save(purchase);
-        session.getTransaction().commit();
-    }
+		return list;
+	}
 
-    @Override
-    public void updatePurchase(Purchase purchase) {
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Purchase p = session.get(Purchase.class, purchase.getId());
-        p.setProduct(purchase.getProduct());
-        p.setSupplier(purchase.getSupplier());
-        p.setQuantity(purchase.getQuantity());
-        p.setDate(purchase.getDate());
-        session.getTransaction().commit();
-    }
+	@Override
+	public Purchase getPurchase(long id) {
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Purchase purchase = session.get(Purchase.class, id);
+		session.getTransaction().commit();
 
-    @Override
-    public void deletePurchase(Purchase purchase) {
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Purchase p = session.get(Purchase.class, purchase.getId());
-        session.delete(p);
-        session.getTransaction().commit();
-    }
-    
+		session.close();
+
+		return purchase;
+	}
+
+	@Override
+	public void savePurchase(Purchase purchase) {
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.save(purchase);
+		session.getTransaction().commit();
+
+		session.close();
+	}
+
+	@Override
+	public void updatePurchase(Purchase purchase) {
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Purchase p = session.get(Purchase.class, purchase.getId());
+		p.setProduct(purchase.getProduct());
+		p.setSupplier(purchase.getSupplier());
+		p.setQuantity(purchase.getQuantity());
+		p.setDate(purchase.getDate());
+		session.getTransaction().commit();
+
+		session.close();
+	}
+
+	@Override
+	public void deletePurchase(Purchase purchase) {
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Purchase p = session.get(Purchase.class, purchase.getId());
+		session.delete(p);
+		session.getTransaction().commit();
+
+		session.close();
+	}
+
 }
